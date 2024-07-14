300ef19475330cbb8179034737626342ec4dfdfc
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-07-14 18:37:12 +0530

    save prompt in sidebar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -18,15 +18,30 @@ const ContextProvider = (props) => {
         },75*index)
     }
 
+    const newChat = () => {
+        setLoading(false)
+        setShowResult(false)
+    }
+
     const onSent = async (prompt) => {
 
         setResultData("")
         setLoading(true)
         setShowResult(true)
-        setRecentPrompt(input)
-        const response = await run(input)
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt);
+            setRecentPrompt(prompt)
+        }
+        else{
+            setPrevPrompts(prev => [...prev,input])
+            setRecentPrompt(input)
+            response = await run(input)
+        }
+        
         let responseArray = response.split("**");
-        let newResponse;
+        let newResponse = "";
+    
         for(let i=0; i < responseArray.length; i++)
         {
             if(i === 0 || i%2 !== 1){
@@ -40,7 +55,7 @@ const ContextProvider = (props) => {
         let newResponseArray = newResponse2.split(" ");
         for(let i=0; i < newResponseArray.length; i++){
             const nextWord = newResponseArray[i];
-            delayPara(i,nextWord+"")
+            delayPara(i,nextWord+" ")
         }
         setLoading(false)
         setInput("")
@@ -50,7 +65,7 @@ const ContextProvider = (props) => {
     
 
     const contextValue = {
-        prevPrompts,setPrevPrompts,onSent,setRecentPrompt,recentPrompt,showResult,loading,resultData,input,setInput
+        prevPrompts,setPrevPrompts,onSent,setRecentPrompt,recentPrompt,showResult,loading,resultData,input,setInput,newChat
     }
     return (
         <Context.Provider value={contextValue}>
